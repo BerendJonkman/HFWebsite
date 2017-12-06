12af43d7d6fa7d2dcf9ee39ebbc1996c4afd4d08
Berend13 <b.jonkman13@gmail.com>
2017-12-06 16:06:02 +0100

    Laatste Repositories toegevoegd.

--- HFWebsiteA7/HFWebsiteA7/Repositories/Classes/RestaurantFoodTypeRepository.cs
+++ HFWebsiteA7/HFWebsiteA7/Repositories/Classes/RestaurantFoodTypeRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using HFWebsiteA7.Models;
 
 namespace HFWebsiteA7.Repositories.Classes
 {
@@ -9,5 +10,19 @@ namespace HFWebsiteA7.Repositories.Classes
     {
         private HFWebsiteA7Context db = new HFWebsiteA7Context();
 
+        public void AddRestaurantFoodType(RestaurantFoodType restaurantFoodType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RestaurantFoodType> GetAllRestaurantFoodTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public RestaurantFoodType GetRestaurantFoodType(int restaurantFoodTypeId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- HFWebsiteA7/HFWebsiteA7/Repositories/Classes/RestaurantRepository.cs
+++ HFWebsiteA7/HFWebsiteA7/Repositories/Classes/RestaurantRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using HFWebsiteA7.Models;
 
 namespace HFWebsiteA7.Repositories.Classes
 {
@@ -9,5 +10,19 @@ namespace HFWebsiteA7.Repositories.Classes
     {
         private HFWebsiteA7Context db = new HFWebsiteA7Context();
 
+        public void AddRestaurant(Restaurant restaurant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Restaurant> GetAllRestaurants()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Restaurant GetRestaurant(int restaurantId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
