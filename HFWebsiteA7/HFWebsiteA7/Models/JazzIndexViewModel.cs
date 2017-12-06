3e6c81dd65487ff3a83901482cdefa90eb85d389
Pim van Ewijk <pv.halfweg@hotmail.com>
2017-12-06 23:40:21 +0100

    Almost finished the Jazz Index page, made a start on the different day detail pages, made a start on the reservation page, on all these pages the dynamic elements are left out

--- HFWebsiteA7/HFWebsiteA7/Models/JazzIndexViewModel.cs
+++ HFWebsiteA7/HFWebsiteA7/Models/JazzIndexViewModel.cs
@@ -7,14 +7,6 @@ namespace HFWebsiteA7.Models
 {
     public class JazzIndexViewModel
     {
-        public List<Day> FestivalDays { get; set; }
-
-        public List<Concert> ThursdayConcerts{ get; set; }
-
-        public List<Concert> FridayConcerts { get; set; }
-
-        public List<Concert> SaturdayConcerts { get; set; }
-
-        public List<Concert> SundayConcerts { get; set; }
+        public List<FestivalDay> FestivalDays { get; set; }
     }
 }
