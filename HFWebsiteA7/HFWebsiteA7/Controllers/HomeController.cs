bb6dd65c011a7bb9c22660aa321bbee6b8209f3e
Pim van Ewijk <pv.halfweg@hotmail.com>
2017-12-12 15:32:42 +0100

    Changed the homecontroller

--- HFWebsiteA7/HFWebsiteA7/Controllers/HomeController.cs
+++ HFWebsiteA7/HFWebsiteA7/Controllers/HomeController.cs
@@ -11,21 +11,8 @@ namespace HFWebsiteA7.Controllers
 {
     public class HomeController : Controller
     {
-        private HFWebsiteA7Context db = new HFWebsiteA7Context();
-        private List<Band> bands = new List<Band>();
-        private List<Event> events = new List<Event>();
-        private List<Concert> concerts = new List<Concert>();
-
-        private List<Restaurant> restaurants = new List<Restaurant>();
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
